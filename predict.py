6e2897dfe9021fa0b4a4e1bd02a4ea5de992ef27
Patrick Bucher <patrick.bucher@mailbox.org>
2020-11-21 16:17:09 +0100

    working training/prediction code (roughtly 61% accuracy)

---
+++ predict.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+weights = np.array([0.30464707, 0.01079016, 0.2445885, 0.87400568])
+
+def main():
+    test = pd.read_csv('csv/test.csv')
+    test = prepare_test(test).to_numpy()
+
+    submission = pd.DataFrame({
+        'PassengerId': np.array([], dtype=np.int),
+        'Survived': np.array([], dtype=np.int),
+    })
+    for i in range(len(test)):
+        id = int(test[i][0])
+        inpt = test[i][1:] # ignore id column
+        pred = inpt.dot(weights)
+        submission = submission.append({
+            'PassengerId': id,
+            'Survived': pred
+        }, ignore_index=True)
+
+    submission['PassengerId'] = submission['PassengerId'].astype(int)
+    submission['Survived'] =  submission['Survived'].round().fillna(0).astype(int)
+    submission.to_csv('submission.csv', index=False)
+
+
+def prepare_test(df):
+    df = pd.DataFrame({
+        'id': df['PassengerId'],
+        'class': df['Pclass'] / max(df['Pclass']),
+        'sex': df['Sex'].map({'male': 0, 'female': 1}),
+        'age': df['Age'].fillna(df['Age'].mean()) / max(df['Age']),
+        'fare': df['Fare'] / max(df['Fare']),
+    })
+    return df
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+
+def main():
+    train = pd.read_csv('csv/train.csv')
+    goals = pd.DataFrame({'survived': train['Survived']})
+    train = prepare_train(train)
+
+    goals = goals.to_numpy()
+    inputs = train.to_numpy()
+
+    weights = np.array([1/4, 1/4, 1/4, 1/4])
+    alpha = 1e-4
+
+    for i in range(10_000):
+        for j in range(len(inputs)):
+            goal = goals[j][0]
+            inpt = inputs[j]
+
+            pred = inpt.dot(weights)
+
+            delta = goal - pred
+            weigh_delta = delta * inpt
+            adjustments = weigh_delta * alpha
+
+            weights += adjustments
+
+    print('weights', weights)
+
+    correct = 0
+    for i in range(len(inputs)):
+        inpt = inputs[j]
+        pred = inpt.dot(weights)
+        if round(pred) == goals[i]:
+            correct += 1
+
+    accuracy = correct / len(inputs) * 100
+    print(f'accuracy: {accuracy:.3f}%')
+
+
+def prepare_train(df):
+    df = pd.DataFrame({
+        'class': df['Pclass'] / max(df['Pclass']),
+        'sex': df['Sex'].map({'male': 0, 'female': 1}),
+        'age': df['Age'] / max(df['Age']),
+        'fare': df['Fare'] / max(df['Fare']),
+    })
+    df['age'] = df['age'].fillna(df['age'].mean())
+    return df
+
+
+if __name__ == '__main__':
+    main()
