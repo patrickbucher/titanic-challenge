0e0f93ecc3ae46710344db6a8d0a03f5bf567ba9
Patrick Bucher <patrick.bucher@seantis.ch>
2020-11-29 09:55:47 +0100

    features: removed ones that are not useful; new feature "child"

--- common.py
+++ common.py
@@ -6,21 +6,20 @@ def prepare(orig, with_id=False):
         'class': normalize(orig['Pclass']),
         'sex': orig['Sex'].map({'male': 0, 'female': 1}),
         'age': normalize(orig['Age']),
-        'gentleman': ((orig['Pclass'] == 1) & (orig['Sex'] == 'male')).astype(int),
         'wife': orig['Name'].str.contains('Mrs.').astype(int),
-        'fare': normalize(orig['Fare']),
-        'sibsp': normalize(orig['SibSp']),
-        'parch': normalize(orig['Parch']),
-        'cabin': orig['Cabin'].str.match('[ABC][0-9]+').fillna(False).astype(int),
+        'cabin': orig['Cabin'].str.match(r'\w+').fillna(False).astype(int),
         'embarked': orig['Embarked'].map({
             'S': 0.336957,
             'C': 0.553571,
             'Q': 0.389610,
-        }), # survival rates from features.py
+        }),  # survival rates from features.py
     })
 
     # missing age: assume average age
     df['age'] = df['age'].fillna(df['age'].mean())
+
+    age = orig['Age'].fillna(0)
+    df['child'] = ((age < 16) & (age != 0)).astype(int)
 
     df['embarked'] = df['embarked'].fillna(0)
 
@@ -31,6 +30,7 @@ def prepare(orig, with_id=False):
         df = df[cols]
 
     return df
+
 
 def normalize(v):
     return (v - v.mean()) / v.std()
