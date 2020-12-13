4dbb2efc571b4ec876c95df5ecc43ec5b4bf0178
Patrick Bucher <patrick.bucher@mailbox.org>
2020-12-13 13:41:25 +0100

    hold-out validation

--- train_logistic_regression.py
+++ train_logistic_regression.py
@@ -14,22 +14,33 @@ def cost(x, y, theta):
 
 
 def main():
-    train = pd.read_csv('csv/train.csv')
-    goals = pd.DataFrame({'survived': train['Survived']})
+    data = pd.read_csv('csv/train.csv')
+
+    # shuffle and split (0.7/0.3)
+    data = data.sample(frac=1)
+    m = len(data)
+    m_train = round(m * 0.7)
+    train = data.iloc[:m_train,]
+    valid = data.iloc[m_train:,]
+
+    goals_train = pd.DataFrame({'survived': train['Survived']})
+    goals_valid = pd.DataFrame({'survived': valid['Survived']})
     train = prepare(train)
-    print(train)
+    valid = prepare(valid)
 
-    goals = goals.to_numpy()
-    inputs = train.to_numpy()
+    goals_train = goals_train.to_numpy()
+    goals_valid = goals_valid.to_numpy()
+    inputs_train = train.to_numpy()
+    inputs_valid = valid.to_numpy()
 
-    theta = np.zeros((inputs.shape[1], 1))
+    theta = np.zeros((inputs_train.shape[1], 1))
     alpha = 0.01
     iterations = 100_000
 
-    y = goals
+    y = goals_train
     for i in range(iterations):
-        m = len(inputs)
-        x = inputs
+        m = len(inputs_train)
+        x = inputs_train
         h = x.dot(theta)
         p = sigmoid(h)
         theta -= (alpha/m) * x.transpose().dot((p - y))
@@ -38,11 +49,11 @@ def main():
             print(f'cost={c:.3f}')
 
 
-    predictions = sigmoid(x.dot(theta))
+    predictions = sigmoid(inputs_valid.dot(theta))
     predictions[:,0] = predictions[:,0].round()
-    correct = len(inputs[(predictions == goals)[:,0]])
+    correct = len(inputs_valid[(predictions == goals_valid)[:,0]])
     
-    accuracy = correct / len(goals) * 100
+    accuracy = correct / len(goals_valid) * 100
     print(f'accuracy: {accuracy:.3f}%')
 
     weights_file = 'weights.csv'
